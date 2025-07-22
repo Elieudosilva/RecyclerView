8b52839838647b96ba65f32f0ac49e4773d906a0
Elieudo Silva <elieudosilva95@gmail.com>
2025-07-21 22:20:07 -0300

    ultima tela do app

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.devspace.recyclerview
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -20,19 +24,30 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        // Set Adapter
-        // Lianer layout manager
-        //Submeter a lista
-
-
         val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val ivList = findViewById<ImageView>(R.id.iv_list)
+        val ivGrid= findViewById<ImageView>(R.id.iv_grid)
         val adapter = ContactListAdapter()
 
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
-
         adapter.submitList(contacts)
 
+        ivGrid.setOnClickListener {
+            rvList.layoutManager = GridLayoutManager(this,2)
+        }
+
+        ivList.setOnClickListener {
+            rvList.layoutManager = LinearLayoutManager(this)
+        }
+
+        adapter.setOnClickListener { contact ->
+            val intent = Intent(this,ContactDetailActivity::class.java)
+            intent.putExtra("name", contact.name)
+            intent.putExtra("phone", contact.phone)
+            intent.putExtra("icon", contact.icon)
+            startActivity(intent)
+        }
     }
 }
 
@@ -40,82 +55,84 @@ private val contacts = listOf(
     Contact(
         name = "Ana",
         phone = "(55) 11 987479834",
-        R.drawable.sample1
+        icon = R.drawable.sample1
     ),
     Contact(
         name = "Rafael",
         phone = "(55) 11 987475284",
-        R.drawable.sample2
+        icon =   R.drawable.sample2
+
+
 ),
     Contact(
         name = "Maria",
         phone = "(55) 11 987420364",
-        R.drawable.sample3
+        icon = R.drawable.sample3
     ),
     Contact(
         name = "Anne",
         phone = "(55) 11 920189834",
-        R.drawable.sample4
+        icon = R.drawable.sample4
     ),
     Contact(
         name = "Jaqueline",
         phone = "(55) 11 989479866",
-        R.drawable.sample5
+        icon = R.drawable.sample5
     ),
     Contact(
         name = "Dada",
         phone = "(55) 11 987665534",
-        R.drawable.sample6
+        icon = R.drawable.sample6
     ),
     Contact(
         name = "Yan",
         phone = "(55) 11 987478877",
-        R.drawable.sample7
+        icon = R.drawable.sample7
     ),
     Contact(
         name = "Silva",
         phone = "(55) 11 984479004",
-        R.drawable.sample8
+        icon = R.drawable.sample8
     ),
     Contact(
         name = "David",
         phone = "(55) 11 987422843",
-        R.drawable.sample9
+        icon = R.drawable.sample9
     ),
     Contact(
         name = "Francisco",
         phone = "(55) 11 987505034",
-        R.drawable.sample10
+        icon = R.drawable.sample10
     ),
     Contact(
         name = "keyla",
         phone = "(55) 11 987779830",
-        R.drawable.sample11
+        icon = R.drawable.sample11
     ),
     Contact(
         name = "Felipe",
         phone = "(55) 11 987309800",
-        R.drawable.sample12
+        icon = R.drawable.sample12
     ),
     Contact(
         name = "Viviane",
         phone = "(55) 11 999479833",
-        R.drawable.sample13
+        icon = R.drawable.sample13
     ),
     Contact(
         name = "Douglas",
         phone = "(55) 11 987477775",
-        R.drawable.sample14
+        icon = R.drawable.sample14
     ),
     Contact(
         name = "Val",
         phone = "(55) 11 987479955",
-        R.drawable.sample15
+        icon = R.drawable.sample15
     ),
     Contact(
         name = "Jessica",
         phone = "(55) 11 989470034",
-        R.drawable.sample16
+        icon = R.drawable.sample16
     )
 
 )
